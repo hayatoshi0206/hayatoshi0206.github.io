e72963e74a9259f9646c6c3fccef7ff61ec337cc
Toshiki Hayashi <toshiki.hayashi@datadoghq.com>
2022-08-18 14:56:46 +0900

    adding my index.html

---
+++ index.js
@@ -0,0 +1,17 @@
+import { datadogRum } from '@datadog/browser-rum';
+
+datadogRum.init({
+    applicationId: '5794a062-2dc3-4911-9ce9-948fe9deb7cb',
+    clientToken: 'pub227c7d1ed40e56f6e705f5c0e9c7eb97',
+    site: 'datadoghq.com',
+    service:'myapplication',
+    
+    // Specify a version number to identify the deployed version of your application in Datadog 
+    // version: '1.0.0',
+    sampleRate: 100,
+    premiumSampleRate: 100,
+    trackInteractions: true,
+    defaultPrivacyLevel:'mask-user-input'
+});
+    
+datadogRum.startSessionReplayRecording();
